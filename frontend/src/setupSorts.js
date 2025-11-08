486a5f2ee9c32f079b5f2fb3ba4f6085908800b0
Darkx32 <Darkx32@users.noreply.github.com>
2025-11-07 21:00:33 -0300

    [Feature] Add quick sort and info texts

--- frontend/src/setupSorts.js
+++ frontend/src/setupSorts.js
@@ -3,8 +3,18 @@ function isSorted(data) {
 }
 
 function runBubble(data) {
-    return window.WASM.sort_test(data);
+    return window.WASM.bubble_sort(data);
+}
+
+function runQuick(data) {
+    return window.WASM.quick_sort(data);
+}
+
+function resetQuick() {
+    window.WASM.reset_quick_sort_state();
 }
 
 window.isSorted = isSorted;
 window.runBubble = runBubble;
+window.runQuick = runQuick;
+window.resetQuick = resetQuick;
