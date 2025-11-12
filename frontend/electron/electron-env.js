1acb8aa7b65a29d79d710a390a1dd5d25735935a
Bielsilvafernandes <gabrielfernandosilvafernandes@gmail.com>
2025-11-12 17:24:14 -0300

    Documentação front
    ARQUIVO ELECTRON-ENV.JS

--- frontend/electron/electron-env.js
+++ frontend/electron/electron-env.js
@@ -1,3 +1,8 @@
+
+/**
+ * Declara o namespace global NodeJS, que adiciona as variáveis personalizadas à interface `ProcessEnv`.
+ */
+
 declare namespace NodeJS {
   interface ProcessEnv {
     DIST: string
@@ -5,6 +10,25 @@ declare namespace NodeJS {
   }
 }
 
+ /**
+   * @interface ProcessEnv
+   * @description Define as variáveis de ambiente personalizadas acessíveis através de `process.env`.
+   * @property {string} DIST - Um caminho absoluto para a pasta de distribuição (build output).
+   * @property {string} VITE_PUBLIC - URL pública usada pelo Vite para servir recursos estáticos.
+   */
+
+
+
 interface Window {
+  /**
+ * Estende o objeto global `Window` para incluir a API `ipcRenderer` do Electron.
+ * Isso permite a comunicação segura entre o processo principal (Main) e o Renderer que isa o sistema de mensagens do electron
+ */
+
   ipcRenderer: import('electron').IpcRenderer
+  /**
+   * @property {Electron.IpcRenderer} ipcRenderer
+   * @description Instância do `ipcRenderer` do Electron injetada no contexto da janela.
+   * Permite enviar e receber mensagens do processo principal
+   * */
 }
