57f2e2379ab6b976ee72a977a0ab1883b164c718
paulo11r <ph914039@gmail.com>
2025-11-22 19:20:20 -0300

    Documentação main.js; preload.js;preload.mjs;

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -1,3 +1,4 @@
+// importação de modulos do nodejs e electron
 import path from 'path';
 import { fileURLToPath } from 'url';
 import {app, BrowserWindow} from 'electron';
@@ -5,31 +6,37 @@ import {app, BrowserWindow} from 'electron';
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+//certifica que pelo menos uma instância do aplicativo seja executada
 if (!app.requestSingleInstanceLock()) {
   app.quit();
   process.exit(0);
 }
 
-let win;
+let win; //variável usada para armezenar a janela local  
+
+//função usada para iniciar a janela de produção
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 800,   //largura da janela
+    height: 600,  //altura da jenala
     webPreferences: {
-      preload: path.join(dirname, "preload.js"),
+      preload: path.join(dirname, "preload.js"),   //roda a aplicação antes do renderer 
       nodeIntegration: true,
       contextIsolation: true
     },
   });
 
+  //quamdo o coteúdo da janela terminar de carregar, ele manda uma mensagem ipc ao render 
   win.webContents.on("did-finish-load", () => {
     win.webContents.send("main-process-message", (new Date).toLocaleString())
   });
 
+  //maximiza a janela assim que terminar de carregar-lá
   win.on("ready-to-show", () => {
     win.maximize();
   });
 
+  //atalho para abrir e fechar DevTools ao pressionar f12
   win.webContents.on("before-input-event", (_, input) => {
     if (input.type == "keyDown" && input.key == "F12") {
       if (win.webContents.isDevToolsOpened())
@@ -37,26 +44,31 @@ function createWindow() {
       else win.webContents.openDevTools();
     }
   });
-
+  
+  //carrega o servidor local com hot-reload , assim que estiver em um ambiente de desenvolvimento(vinte)
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL)
     
   }
-
+  //carrega o arquivo html estático na versão de produção
   win.loadFile('./src/index.html');
+   //remove o menu padrão da jenla 
   win.setMenu(null);
 }
 
+//fecha a aplicação quando todas as janelas forem fechadas 
 app.on('window-all-closed', () => {
+  //mantém a aplicação aberta sem janelas 
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+//recria a janela quando apertar no dock
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
+//inicializa o electon e cria a janela quando estiver pronta 
 app.whenReady().then(createWindow);
--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,30 +1,59 @@
+//importa o contexBridge do electron para expor APIs seguras 
 const { contextBridge } = require("electron");
+//importa manipulação de caminhos
 const path = require("path");
 
+//permite async/await na parte superior
 (async () => {
+
+  //importa o (sorts.js) gerado pelo Emscripten 
   const ModuleFactory = (await import("../../backend/out/sorts.js")).default;
+
+  //caminho para o arquivo WASM compilado
   const wasmPath = "../../backend/out/sorts.wasm";
 
+  //inicia o webAssembly passando as configurações personalizadas 
   const Module = await ModuleFactory({
+    //informa ao emscripten a onde está o .wasm
     locateFile: () => wasmPath
   });
 
+  //cria funções JAVAscript que chamam funcões exportadas do wasm
   const is_sorted = Module.cwrap("is_sorted", "boolean", ["number", "number"]);
+
+  
+  //reset_quick_sort_state -- reseta o estado do quick sort 
   const reset_quick_sort_state = Module.cwrap("reset_quick_sort_state", "void", []);
+
+  //expôem no navegar de forma global (WINDOw.WASM)
   contextBridge.exposeInMainWorld("WASM", {
+
+    //função que vai executar o bubble sort usando webassembly
     bubble_sort: (data) => {
+
+      //vai calcular quantos bytes vão ser usados para armazenar o array
       const sBytes = data.length * Int32Array.BYTES_PER_ELEMENT;
+
+      
+      //aloca a memória no webassembly
       const ptr = Module._malloc(sBytes);
 
+      //pega os dados do jsavascript e copia esses dados para o heap wasm
       Module.HEAP32.set(data, ptr / Int32Array.BYTES_PER_ELEMENT);
+
+      //chama a função -bubble_sort dentro do webassembly
       Module._bubble_sort(ptr, data.length);
 
       const result = new Int32Array(Module.HEAP32.buffer, ptr, data.length).slice();
 
+      //libera a mémoria alocado
       Module._free(ptr);
 
+      //retorna uma cópia em formato array javascript
       return Array.from(result);
     },
+
+    //função que executa o quicksort usando webassembly
     quick_sort: (data) => {
       const sBytes = data.length * Int32Array.BYTES_PER_ELEMENT;
       const ptr = Module._malloc(sBytes);
@@ -32,22 +61,31 @@ const path = require("path");
       Module.HEAP32.set(data, ptr / Int32Array.BYTES_PER_ELEMENT);
       Module._quick_sort(ptr, data.length);
 
+      //ler o array ordenado acima
       const result = new Int32Array(Module.HEAP32.buffer, ptr, data.length).slice();
 
       Module._free(ptr);
 
       return Array.from(result);
     },
+
+    //função que verifica se o array está ordenando via WebAssembly
     is_sorted: (data) => {
       const sBytes = data.length * Int32Array.BYTES_PER_ELEMENT;
       const ptr = Module._malloc(sBytes);
 
       Module.HEAP32.set(data, ptr / Int32Array.BYTES_PER_ELEMENT);
+
+      //chama a função is_sorted (ptr, length)
       const result = is_sorted(ptr, data.length);
 
+      //vai liberar memória
       Module._free(ptr);
+      //retorna true ou falso
       return result;
     },
+    
+    //reserta qualquer estado interno usado pelo quicksort no WebAssembly
     reset_quick_sort_state: () => {
       reset_quick_sort_state()
     }
